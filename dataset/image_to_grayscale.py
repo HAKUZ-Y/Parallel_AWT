e83bda108b20a3175d2e0ceea9b69c743830faea
hakuz <youhengyzzz@gmail.com>
2025-04-13 12:36:22 -0400

    add reconstructed folder

--- dataset/image_to_grayscale.py
+++ dataset/image_to_grayscale.py
@@ -9,6 +9,9 @@ img = Image.open(input_file + ".jpg").convert("L")
 img = img.resize((size, size))
 arr = np.array(img)
 
+# generate grayscale txt
 np.savetxt(input_file + "_" + str(size) + "_" + ".txt", arr, fmt="%.1f")
+
+# generate grayscale image
 cv2.imwrite("restored.jpg", arr.astype(np.uint8), [cv2.IMWRITE_JPEG_QUALITY, 95])
 
