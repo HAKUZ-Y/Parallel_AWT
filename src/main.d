5441bf9a8f098d409eedcbff0e57ea7761073c59
hakuz <youhengyzzz@gmail.com>
2025-04-25 23:26:59 -0400

    inplace, achieve perfect speedup

---
+++ src/main.d
@@ -0,0 +1,7 @@
+main.o: main.cpp awt_mpi.hpp awt_common.hpp \
+  /opt/homebrew/opt/libomp/include/omp.h awt_shared.hpp metrics.hpp
+awt_mpi.hpp:
+awt_common.hpp:
+/opt/homebrew/opt/libomp/include/omp.h:
+awt_shared.hpp:
+metrics.hpp:
